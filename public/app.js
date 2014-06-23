c0b840005dcc4c8e0b6dcd8a716fa8f59c6241ea
Derek Smith(Home) <dsmith@creativeworx.com>
2014-06-23 12:29:13 -0400

    Removed minified files for dev and removed erroneous app.js files.  Versioned un-minified vendor libs

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 angular.module('MyApp', ['ngCookies', 'ngResource', 'ngMessages', 'ngRoute', 'mgcrea.ngStrap'])
-  .config(function(['$locationProvider',$locationProvider,$routeProvider) {
+  .config(['$locationProvider','$routeProvider', function($locationProvider,$routeProvider) {
         $locationProvider.html5Mode(true);
 
         $routeProvider
